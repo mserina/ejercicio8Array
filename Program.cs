f3f8445f20194179e71668ce649c2e409c2b6cf7
DESKTOP-PR2BK7P\csi23-mserina <csi23-mserina@colegioaltair.net>
2024-01-17 12:45:30 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ejercicio8Array{
+
+    /// <summary>
+    /// 
+    /// msm - 170124
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int indice;
+            int condicion;
+            bool cerrarBucle = false;
+
+            string[] palabras = { "Hola", "Adios","Nuevo", "Viejo", "Vamos"};
+           
+            indice = Array.IndexOf(palabras,"Adios"); //si existe mas de uno solo nos dara la primera posicion
+
+            if (indice < 0)
+            {
+                Console.WriteLine("No existe el el array");
+            }
+            else
+            {
+                Console.WriteLine("La posicion es " + indice);
+            }
+
+            //Establece un condicional que en función de la palabra que pase un usuario por consola
+            Console.WriteLine(" ");
+            Console.WriteLine("Inserte la palabra que desea encontrar");
+            string palabraUsuario = Console.ReadLine();
+            indice = Array.IndexOf(palabras, palabraUsuario);
+
+            if (indice >= 0) 
+            {
+                Console.WriteLine("Existe en el array");
+            }
+            else
+            {
+                Console.WriteLine("No existe en el array");
+            }
+
+
+
+
+        }
+    }
+}
